84e840386b7662c0f1c33b2564d3e87b005beb32
Krzysztof Łazarz <krzysztof.lazarz.dev@gmail.com>
2024-09-29 00:16:17 +0200

    fixed endline

--- create_model.sh
+++ create_model.sh
@@ -2,4 +2,4 @@
 # Bash script to create a model using ollama
 #add 5 seconds sleep to wait for the model to be created
 nohup ollama serve &
-ollama create Lexi-Llama-3-8B-Uncensored_Q8_0.gguf -f hackyeah2024/RAG-System/models/Lexi-Llama-3-8B-Uncensored_Q8_0
+# ollama create Lexi-Llama-3-8B-Uncensored_Q8_0.gguf -f hackyeah2024/RAG-System/models/Lexi-Llama-3-8B-Uncensored_Q8_0
