29ccbf9ffd719cc225b6d8057dbc5b15d76ee6d0
lukasz <q@q.pl>
2024-09-29 03:49:23 +0200

    massive changes

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import logo from '../logo.jpg'
+import {useNavigate} from "react-router-dom";
 function Header() {
+    const navigate = useNavigate();
     return (
         <Box
             sx={{
@@ -22,7 +24,8 @@ function Header() {
                 <img
                     src={logo}
                     alt="gov.pl logo"
-                    style={{ height: '50px' }} // Adjust size as necessary
+                    style={{ height: '50px' }}
+                    onClick={() => navigate("/chat")}// Adjust size as necessary
                 />
             </Box>
 
